e604a8d91c308fd9e41c0135de28b03ecc80282a
fuadhasanshihab <85599261+fuadhasanshihab@users.noreply.github.com>
2021-09-21 17:55:49 +0600

    Create masonry.js

---
+++ masonry.js
@@ -0,0 +1,14 @@
+jQuery('#portfolio').mixItUp({  
+
+  selectors: {
+    target: '.tile',
+    filter: '.filter',
+    sort: '.sort-btn'
+  },
+  
+      animation: {
+      animateResizeContainer: false,
+      effects: 'fade scale'
+    }
+
+});
